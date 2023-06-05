4fc23b722232d644236cd923dc96933c1d8719dd
thi-coutinho <thi.coutinho.santos@gmail.com>
2023-06-05 17:32:34 -0300

    initial server and database config

---
+++ src/app.ts
@@ -0,0 +1,31 @@
+// import 'reflect-metadata';
+import 'express-async-errors';
+import express, { Express } from 'express';
+import cors from 'cors';
+
+// import { redisConnect } from './config/redisconfig';
+import { loadEnv, connectDb, disconnectDB } from '@/config';
+
+loadEnv();
+
+// import { handleApplicationErrors } from '@/middlewares';
+// import {} from '@/routers';
+
+const app = express();
+app
+  .use(cors())
+  .use(express.json())
+  .get('/health', (_req, res) => res.send('OK!'));
+//   .use('/users', usersRouter)
+//   .use(handleApplicationErrors);
+
+export function init(): Promise<Express> {
+  connectDb();
+  return Promise.resolve(app);
+}
+
+export async function close(): Promise<void> {
+  await disconnectDB();
+}
+
+export default app;
